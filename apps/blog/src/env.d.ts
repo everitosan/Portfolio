3e96a36f8c018a7b55dcdde30de8dbbb1b5af3e3
everardo <everardo.ipn@gmail.com>
2026-08-14 21:31:46 -0600

    feat: add umami conffeat: add umami conf

--- apps/blog/src/env.d.ts
+++ apps/blog/src/env.d.ts
@@ -6,6 +6,8 @@ declare module '@fontsource-variable/*'
 interface ImportMetaEnv {
   readonly POCKETBASE_URL: string
   readonly POCKETBASE_PUBLIC_URL: string
+  readonly PUBLIC_UMAMI_URL?: string
+  readonly PUBLIC_UMAMI_WEBSITE_ID?: string
 }
 
 interface ImportMeta {
